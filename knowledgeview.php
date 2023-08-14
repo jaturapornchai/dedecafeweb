9ffe16e715a284ff561d72ac95c0ddd939046331
jaturapornchai <jaturapornchai@gmail.com>
2023-08-15 05:04:23 +0700

    zzz

--- knowledgeview.php
+++ knowledgeview.php
@@ -1,7 +1,67 @@
 <?php
 $title = "DeDe Pos Cafe : Knowledge";
+$page = "";
+$keyword = "";
 if (isset($_GET['page'])) {
-    $title = $_GET['page'];
+    $page = $_GET['page'];
+    $file = 'knowledge/' . $page . '.html';
+    if (file_exists($file)) {
+        $content = file_get_contents($file);
+        $content = str_replace("\n", "<br/>", $content);
+        $contentkeyword = strip_tags($content);
+        $content = str_replace('สั่งอาหาร', '<span>สั่งอาหาร</span>', $content);
+        $content = str_replace('โปรแกรม', '<span>โปรแกรม</span>', $content);
+        $content = str_replace('ร้านอาหาร', '<span>ร้านอาหาร</span>', $content);         
+    }
+}
+
+$filename = 'dictionary.txt';
+
+$dictionary = array();
+
+if (file_exists($filename)) {
+    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        $dictionary[] = $line;
+    }
+    foreach ($dictionary as $word) {
+        $word = trim($word);
+        // length > 10
+        if (strlen($word) > 10) {
+            $contentkeyword = str_replace($word, '|' . $word , $contentkeyword);
+        }
+    }
+    $contentkeyword = str_replace('<br/>', '|' , $contentkeyword);
+    $contentkeyword = str_replace('(', '|' , $contentkeyword);
+    $contentkeyword = str_replace(')', '|' , $contentkeyword);
+    $contentkeyword = str_replace(' ', '|' , $contentkeyword);
+    $contentkeyword = str_replace(' ', '|' , $contentkeyword);
+    $contentkeyword = str_replace('.', '|' , $contentkeyword);
+    $contentkeyword = str_replace(',', '|' , $contentkeyword);
+    $contentkeyword = str_replace('||', '|' , $contentkeyword);
+    $contentkeyword = str_replace('||', '|' , $contentkeyword);
+    // to array
+    $contentkeyword = explode('|', $contentkeyword);
+    // remove if length < 15
+    foreach ($contentkeyword as $key => $word) {
+        if (strlen($word) < 20) {
+            unset($contentkeyword[$key]);
+        }
+    }
+    // count keyword and sort
+    $countkeyword = array();
+    foreach ($contentkeyword as $word) {
+        $word = trim($word);
+        if (isset($countkeyword[$word])) {
+            $countkeyword[$word] += 1;
+        } else {
+            $countkeyword[$word] = 1;
+        }
+    }
+    arsort($countkeyword);
+    // top ten
+    $countkeyword = array_slice($countkeyword, 0, 10);
+    $keyword = implode(',', array_keys($countkeyword));
 }
 include('header.php');
 ?>
--- recipeview.php
+++ recipeview.php
@@ -2,6 +2,67 @@
 $title = "DeDe Pos Cafe Recipe";
 $page = "";
 $keyword = "";
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    $file = 'recipe/' . $page . '.html';
+    if (file_exists($file)) {
+        $content = file_get_contents($file);
+        $content = str_replace("\n", "<br/>", $content);
+        $contentkeyword = strip_tags($content);
+        $content = str_replace('สั่งอาหาร', '<span>สั่งอาหาร</span>', $content);
+        $content = str_replace('โปรแกรม', '<span>โปรแกรม</span>', $content);
+        $content = str_replace('ร้านอาหาร', '<span>ร้านอาหาร</span>', $content);         
+    }
+}
+
+$filename = 'dictionary.txt';
+
+$dictionary = array();
+
+if (file_exists($filename)) {
+    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        $dictionary[] = $line;
+    }
+    foreach ($dictionary as $word) {
+        $word = trim($word);
+        // length > 10
+        if (strlen($word) > 10) {
+            $contentkeyword = str_replace($word, '|' . $word , $contentkeyword);
+        }
+    }
+    $contentkeyword = str_replace('<br/>', '|' , $contentkeyword);
+    $contentkeyword = str_replace('(', '|' , $contentkeyword);
+    $contentkeyword = str_replace(')', '|' , $contentkeyword);
+    $contentkeyword = str_replace(' ', '|' , $contentkeyword);
+    $contentkeyword = str_replace(' ', '|' , $contentkeyword);
+    $contentkeyword = str_replace('.', '|' , $contentkeyword);
+    $contentkeyword = str_replace(',', '|' , $contentkeyword);
+    $contentkeyword = str_replace('||', '|' , $contentkeyword);
+    $contentkeyword = str_replace('||', '|' , $contentkeyword);
+    // to array
+    $contentkeyword = explode('|', $contentkeyword);
+    // remove if length < 15
+    foreach ($contentkeyword as $key => $word) {
+        if (strlen($word) < 20) {
+            unset($contentkeyword[$key]);
+        }
+    }
+    // count keyword and sort
+    $countkeyword = array();
+    foreach ($contentkeyword as $word) {
+        $word = trim($word);
+        if (isset($countkeyword[$word])) {
+            $countkeyword[$word] += 1;
+        } else {
+            $countkeyword[$word] = 1;
+        }
+    }
+    arsort($countkeyword);
+    // top ten
+    $countkeyword = array_slice($countkeyword, 0, 10);
+    $keyword = implode(',', array_keys($countkeyword));
+}
 include('header.php');
 ?>
 
@@ -9,23 +70,10 @@ include('header.php');
     <?php include('menu.php'); ?>
     <div class="container" style="margin-top: 100px;font-size: 12px;">
         <?php
-        if (isset($_GET['page'])) {
-            $page = $_GET['page'];
-            $file = 'recipe/' . $page . '.html';
-            if (file_exists($file)) {
-                $content = file_get_contents($file);
-                $content = str_replace("\n", "<br/>", $content);
-                echo "<h1>$page</h1><br/>";                
-                echo "<div style='font-size: 24px;'>";
-                $content = str_replace('สั่งอาหาร', '<span>สั่งอาหาร</span>', $content);
-                $content = str_replace('โปรแกรม', '<span>โปรแกรม</span>', $content);
-                $content = str_replace('ร้านอาหาร', '<span>ร้านอาหาร</span>', $content);
-                echo $content;
-                echo "</div>";
-            } else {
-                echo 'File not found';
-            }
-        }
+        echo "<h1>$page</h1><br/>";                
+        echo "<div style='font-size: 24px;'>";
+        echo $content;
+        echo "</div>";
         echo "<br/><br/>";
         $files = scandir('recipe');
         foreach ($files as $file) {
@@ -34,7 +82,6 @@ include('header.php');
             echo '<a href="recipeview?page=' . $file . '">' . $file . '</a> : ';
         }
         ?>
-        <br />
     </div>
     <?php include_once('footer.php'); ?>
 
